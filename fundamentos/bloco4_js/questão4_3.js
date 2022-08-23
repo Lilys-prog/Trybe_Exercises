4ff78eaaba2d3e5b8aefa2732d7857aab47b17b4
Lily Silva <lilys.prog@gmail.com>
2022-08-23 08:30:12 -0300

    anotação da aula

---
+++ fundamentos/bloco4_js/questão4_3.js
@@ -0,0 +1,15 @@
+// 5 
+//Crie um algoritmo que recebe a idade de Noel, Ellen e 
+//Nasc e imprime quem é a pessoa mais nova no formato:
+// "Pessoa" é a mais nova.
+
+
+let pessoa = ['Noel', 'Ellen', 'Nasc'];
+let id_ellen = 15;
+let id_nasc = 15;
+let id_noel = 21;
+
+for(let i = 0; i < 3; i += 1){  
+  if   
+}
+
