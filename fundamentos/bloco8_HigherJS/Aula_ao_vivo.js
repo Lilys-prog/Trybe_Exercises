1af61055986a676b34605c161717213055c87b79
Lily Silva <lilys.prog@gmail.com>
2022-09-30 08:57:39 -0300

    estudando

--- fundamentos/bloco8_HigherJS/Aula_ao_vivo.js
+++ fundamentos/bloco8_HigherJS/Aula_ao_vivo.js
@@ -25,7 +25,7 @@ const layoutPage = () => {
 // Uma FUNÇÃO tb pode ser um parâmetro e ela será uma função 'callback' ou 'callback function'. A função que tiver outra função como parâmetro será uma HOF. Também será considerada uma hof uma função que tiver outra como retorno.
 // addEventListener é uma hof integrada do js
 // a função hof não tem 'controle' de quando a callback será executada, vai depender do código,, ou do evento de disparo
-
+// quando não se coloca os parâmetros de uma função (mesmo os () vazios) é pq não se quer que a função seja executada no momento em que a linha for lida. Ela tem alguma condição qque deve ser preenchida (tipo um evento) e não pode ser chamada imediatamente, tem qque esperar. Daí a exclusão dos parẽnteses de argumento (mesmo se a função não pedir argumento e for ()).
 
 // =======================================================================
 // =======================================================================
@@ -42,8 +42,11 @@ const layoutPage = () => {
 //   }
 // };
 
+
+// greetingPeople() não é uma hof, pois não tem função nem no papel de parâmetro e nem no de retorno
 const greetingPeople = () => {
   return "Oláááááá,, enfermeira!";
 }
 
+module.exports = {greetingPeople};
 // module.exports = {greeting, defaultGreeting, badMoodGreeting, loveGreeting, person}
---
+++ fundamentos/bloco8_HigherJS/Aula_ao_vivo.test.js
@@ -0,0 +1,13 @@
+// module.exports = {greeting, defaultGreeting, badMoodGreeting, loveGreeting, person} = require('./greeting');
+
+const greetingPeople = require('./Aula_ao_vivo');
+
+// o comando 'describe', além de dar uma breve descrição da bateria de testes que será executada, os agrupa, forma um bloco de testes por função. É uma função que recebe 2 parâmetros, o primeiro sendo uma string com a descrição e o segundo será uma função. Sendo assim, 'describe' também é uma hof padrão do js.
+// o 'test' ou 'it' também é uma hof padrão
+
+describe('Testa a função greetingPeople', () => {
+  it('Veriifica se ao ser chamada a função retorna a mensagem `Oláááááá,, enfermeira!`', () => {
+    expect(greetingPeople()).toBe('Oláááááá,, enfermeira!');
+
+  })
+})
