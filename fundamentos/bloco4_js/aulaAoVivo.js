f0e59018ee3defdc1e27dbefbf5fcff4ee6fe3cc
Lily Silva <lilys.prog@gmail.com>
2022-10-15 10:42:50 -0300

    testando alias

---
+++ fundamentos/bloco4_js/aulaAoVivo.js
@@ -0,0 +1,21 @@
+// AULA DA TURMA 26B COM O NOEL
+
+// o js é uma linguagem de 'interpretador' pq ela pode ser executada no ambiente de criação, ou seja, não é 'compilada'. O interpretador do js é, por natureza, o browser. Fora do ambiiente web, é o node.
+
+// o code runner permite a integração do código para que sejam 'testados' e para que se mostre a interpretação (a execução). Serve para várias linguagens de programação diferentes, que rodem no vsc.
+
+// js no browser, inspeciona-se qualquer página e utiliza-se o console
+
+
+// Debugger
+
+// usa-se muito o console.log para checar se o código está funcionando, mas através do debugger é possível saber o que está acontecendo com o código a cada momento e detectar erros
+
+let turma = 26;
+let turmaVaiDarCerto = true;
+let faustinho = 'Ô loquinho, meu!';
+
+// debugger > executar e depurar > node.js (ou chrome se for pra depurar no navegador)
+// marca-se então, usando as bolinhas vermelhas das linhas o intervalo que se quer testar (breakpoint). Então clica-se em executar e depurar e ele vai fazer isso linha a linha (dá pra usar como teste de mesa)
+
+
