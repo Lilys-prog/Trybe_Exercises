000fdff885889e2bf79588bfe13f30b1c04344c6
Lily Silva <lilys.prog@gmail.com>
2022-10-15 16:39:46 -0300

    dia 2 do 4

---
+++ fundamentos/bloco4_js/dia42/exArray1.js
@@ -0,0 +1,53 @@
+
+// EXERCÍCIO 01
+// Obtenha o valor “Serviços” do array menu:
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+
+
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+
+
+// EXERCÍCIO 2
+// Procure o índice do valor “Portfólio” do array menu:
+let menu1 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu1.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+
+
+
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+
+
+// EXERCÍCIO 3
+// Adicione o valor “Contato” no final do array menu:
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu2.push('Contato');
+console.log(menu2);
+
+
+
+
+
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+//                =====================
+
+
