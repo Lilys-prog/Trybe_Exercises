ab65c32b25033ec277ed2ab169f8615b5a3d81cb
Lily Silva <lilys.prog@gmail.com>
2022-11-18 14:22:56 -0300

    adicionando o dia 4.3

---
+++ fundamentos/bloco4_js/dia43/exDiia43.js
@@ -0,0 +1,28 @@
+// EXERCÍCIOS DO DIA 4.3
+
+// Exercício 1
+// crie um algoritmo que imprima na tela o fatorial de 10.
+
+let fat = 1;
+
+for (let i = 10; i > 0; i -= 1) {
+  fat *= i;
+}
+console.log(fat);
+
+
+//                =======================================
+//                =======================================
+//                =======================================
+//                =======================================
+//                =======================================
+
+
+// Exercício 2
+// Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra.
+
+let word = 'tryber';
+for (let i = 0; i < word.length; i += 1) {
+  
+}
+
