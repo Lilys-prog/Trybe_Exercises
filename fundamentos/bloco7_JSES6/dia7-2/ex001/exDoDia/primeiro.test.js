3e7140cc4f735f1d1bc0f66db03e23e875ff99cd
Lily Silva <lilys.prog@gmail.com>
2022-11-24 18:38:39 -0300

    começando segundo exercício

--- fundamentos/bloco7_JSES6/dia7-2/ex001/exDoDia/primeiro.test.js
+++ fundamentos/bloco7_JSES6/dia7-2/ex001/exDoDia/primeiro.test.js
@@ -5,8 +5,20 @@
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado.
 
+const myRemove = require('./primeiro.js');
 const remove = require('./primeiro.js');
 
 describe('Testes da função myRemove', () => {
-
+  it('Remover o 3', () => {
+    const arr = [1, 2, 3, 4];
+    expect([1, 2, 4]).toEqual(myRemove(arr, 3));
+  });
+  it('Ao remover, a função realmente remove', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 3)).not.toEqual(arr);
+  });
+  it('Ao remover o 5, o array permanece o mesmo', () => {
+    const arr = [1, 2, 3, 4];
+    expect(myRemove(arr, 5)).toEqual(arr);
+  });
 });
