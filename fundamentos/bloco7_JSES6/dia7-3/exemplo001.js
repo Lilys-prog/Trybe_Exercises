c7d24bf9159c59cf2d60176a5e89cfa946e17146
Lily Silva <lilys.prog@gmail.com>
2022-11-26 20:07:23 -0300

    primeiro exeemplo

---
+++ fundamentos/bloco7_JSES6/dia7-3/exemplo001.js
@@ -0,0 +1,13 @@
+const skills = ['HTML', 'CSS', 'JavaScript'];
+
+const addNewSkill = (newSkill) => {
+  skills.push(newSkill);
+}
+
+//aqui entrou o teste
+test('Adiciona Jest na lista de habilidades', () => {  
+  const previousLength = skills.length;
+  addNewSkill('Jest');
+  expect(skills).toContain('Jest');
+  expect(skills).toHaveLength(previousLength + 1);
+});
