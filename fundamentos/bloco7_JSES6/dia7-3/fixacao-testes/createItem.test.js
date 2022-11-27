81e0c87936288cffb056ac7efff5f880bec035ce
Lily Silva <lilys.prog@gmail.com>
2022-11-27 09:40:15 -0300

    primeiiro teste

---
+++ fundamentos/bloco7_JSES6/dia7-3/fixacao-testes/createItem.test.js
@@ -0,0 +1,11 @@
+const createItem = require('./createItem');
+describe('a função createItem', () => {
+  it('cria um item válido', () => {
+    expect(createItem('banana', 'kg', 1.99, 20)).toEqual({ name: 'banana', unit: 'kg', price: 1.99, quantity: 20});
+  });
+  it.todo('utiliza zero como quantidade padrão');
+  it.todo('Lança um erro quando não recebe parâmetros');
+  it.todo('Lança um erro se o nome do item não é uma string');
+  it.todo('Lança um erro se o preço é negativo');
+  it.todo('Lança um erro se o preço é zero');
+});
