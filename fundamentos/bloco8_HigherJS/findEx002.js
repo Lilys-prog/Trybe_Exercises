3ab192236b18bcc74c0399ed4826ec38d997e966
Lily Silva <lilys.prog@gmail.com>
2022-12-01 15:04:33 -0300

    estudando funções HOF de arrays e fazendo exercícios de fixação

---
+++ fundamentos/bloco8_HigherJS/findEx002.js
@@ -0,0 +1,5 @@
+// Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+console.log(names.find((letras) => letras.length === 5));
