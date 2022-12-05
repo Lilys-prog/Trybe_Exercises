a32e4756ec14a791780bc886e3ee2921d0db5a02
Lily Silva <lilys.prog@gmail.com>
2022-12-05 18:23:41 -0300

    adicionei uma pequena anotação sobre objetos

--- fundamentos/bloco7_JSES6/objects.js
+++ fundamentos/bloco7_JSES6/objects.js
@@ -1,4 +1,3 @@
-
 // // objeto customer, possui pares chave-valor
 // const customer = {
 //   firstName: 'Roberto',
@@ -6,7 +5,7 @@
 //   job: 'Teacher',
 // };
 
-// // para adicionar novas propriedades a um objeto podemos reescrevê-lo ou usar 
+// // para adicionar novas propriedades a um objeto podemos reescrevê-lo ou usar
 // // objeto.nomeDaPropriedade.
 
 // // objeto[variavelQueContemONomeDaPropriedade].
@@ -14,33 +13,27 @@
 // customer.lastname = 'Farias';
 // console.log(customer);
 
-
-
-
-
 const customer = {
-  firstName: 'Roberto',
+  firstName: "Roberto",
   age: 22,
-  job: 'Teacher',
+  job: "Teacher",
 };
 
-let newKey = 'lastName';
-const lastName = 'Ferreira';
+let newKey = "lastName";
+const lastName = "Ferreira";
 customer[newKey] = lastName;
 
-newKey = 'fullName';
+newKey = "fullName";
 const fullName = `${customer.firstName} ${customer.lastName}`;
 customer[newKey] = fullName;
 
 console.log(customer);
 
-
-
 // ========================================
 // ========================================
 // ========================================
 
-// Objetos são um dos tipos de dados em JavaScript que possuem uma estrutura de chave e valor. 
+// Objetos são um dos tipos de dados em JavaScript que possuem uma estrutura de chave e valor.
 // const coolestTvShow = {
 //   name: 'BoJack Horseman',
 //   genre: 'adult animation',
@@ -49,23 +42,23 @@ console.log(customer);
 //   quote: 'Princess Carolyn always lands on her feet.',
 //   seasons: 6,
 // };
-// a vírgula separa cada par. A última, depois do último elemento, é opcional
+// a vírgula separa cada par. A última, depois do último elemento, é opcional, mas o lint recomenda
 
 // os objetos permitem o uso da estrutura de repetição 'for.. in'
 
 const coolestTvShow = {
-  name: 'BoJack Horseman',
-  genre: 'adult animation',
-  createdBy: 'Raphael Bob-Waksberg',
-  favoriteCharacter: 'Princess Carolyn',
-  quote: 'Princess Carolyn always lands on her feet.',
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
   seasons: 6,
 };
 
+// o código abaixo mostra as chaves do objeto:
 // for (const property in coolestTvShow) {
 //   console.log(property);
 // }
-
 
 // de forma mais simples
 console.log(Object.keys(coolestTvShow));
@@ -76,69 +69,66 @@ console.log(Object.keys(coolestTvShow));
 // ========================================
 // ========================================
 
-
 // Tente criar uma função que exiba as habilidades do objeto student. Cada habilidade deve ser exibida no formato “Nome da habilidade, Nível: valor da chave referente à habilidade”. Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução.
 
 // Object.values mostra os valores que são relacionados às chaves, com o seu tipo original, ou seja, se são números, eles vêm em forma de números
 
 const student1 = {
-  Html: 'Muito Bom',
-  Css: 'Bom',
-  JavaScript: 'Ótimo',
-  SoftSkills: 'Ótimo',
+  Html: "Muito Bom",
+  Css: "Bom",
+  JavaScript: "Ótimo",
+  SoftSkills: "Ótimo",
 };
 
-  console.log(student1)
+console.log(student1);
 
-  // =========================================
-  // =========================================
-  // =========================================
+// =========================================
+// =========================================
+// =========================================
 
- // object entries
- // esse comando monta um array no qual os elementos são os pares do objeto
- const coolestTvShow1 = {
-  name: 'BoJack Horseman',
-  genre: 'adult animation',
-  createdBy: 'Raphael Bob-Waksberg',
-  favoriteCharacter: 'Princess Carolyn',
-  quote: 'Princess Carolyn always lands on her feet.',
+// object entries
+// esse comando monta um array no qual os elementos são os pares do objeto (array de arrays)
+const coolestTvShow1 = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
   seasons: 6,
 };
 console.log(Object.entries(coolestTvShow1));
 // abertura do array de saída (6 elementos)
-// [ 
+// [
 //   [ 'name', 'BoJack Horseman' ],
 //   [ 'genre', 'adult animation' ],
 //   [ 'createdBy', 'Raphael Bob-Waksberg' ],
 //   [ 'favoriteCharacter', 'Princess Carolyn' ],
 //   [ 'quote', 'Princess Carolyn always lands on her feet.' ],
 //   [ 'seasons', 6 ]
-// ] 
+// ]
 // fechamento do array de saída
 
 // cada elemento desse array é um array com 2 elementos, a chave e o valor. Então a saída é um array de arrays, onde o primeiro elemento (a chave) é sempre uma string, e o segundo elemento vai ser o tipo que ele for realmente, como um número, por exemplo.
 
-
-
-
 // =========================================
 // =========================================
 // =========================================
 
- // DUAS FORMAS DE FAZER
+// DUAS FORMAS DE FAZER
 
- const países = {
-  França: 'Paris',
-  Brasil: 'Brasília',
-  Espanha: 'Madrid',
-  Portugal: 'Lisboa',
+const países = {
+  França: "Paris",
+  Brasil: "Brasília",
+  Espanha: "Madrid",
+  Portugal: "Lisboa",
 };
 const pairKeyValue = Object.entries(países);
 // console.log(pairKeyValue);
-for(index in pairKeyValue) {
-  console.log('--------');
-  console.log('País:', pairKeyValue[index][0]);
-  console.log('Capital:', pairKeyValue[index][1]);
-};
-// gostei muito dessa segunda forma
 
+for (index in pairKeyValue) {
+  console.log("--------");
+  console.log("País:", pairKeyValue[index][0]);
+  console.log("Capital:", pairKeyValue[index][1]);
+}
+// gostei muito dessa segunda forma, do formato bonito da saída, embora mais complicado de escrever
+// o loop do for/in vai usar [0] e [1] em todos os casos pq é um array de arrays. Ou seja, a cada loop ele vai voltar a contagem do index pra 0. Por isso cada país vai ser sempre [0] e cada capital vai ser sempre [1].
