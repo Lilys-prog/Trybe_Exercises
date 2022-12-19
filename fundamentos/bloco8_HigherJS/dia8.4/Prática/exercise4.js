f6554bff2ff80ec12b0cc8bd8e018161f6111a1f
Lily Silva <lilys.prog@gmail.com>
2022-12-19 09:59:57 -0300

    exercício 4 do dia 4

--- fundamentos/bloco8_HigherJS/dia8.4/Prática/exercise4.js
+++ fundamentos/bloco8_HigherJS/dia8.4/Prática/exercise4.js
@@ -6,3 +6,49 @@
 // Os exercícios destacados com 🚀 são os fundamentais pra você ir bem no projeto! Todos os exercícios vão contribuir com sua formação, mas fique de olho nesses! 👀
 
 // =================
+
+// 🚀 Exercício 4
+  // Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+  // (bornIn: nascido em)
+
+  const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },   
+  ];
+  
+  // Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20
+  // escreva filterPeople abaixo
+
+const person = { name, nationality, bornIn } = people;
+
+const filterPeople = (people) => {
+  return people.filter((person) => {
+    return person.nationality === 'Australian' && (person.bornIn > 1900 && person.bornIn < 2001);
+  });  
+}
+
+console.log(filterPeople(people));
+
