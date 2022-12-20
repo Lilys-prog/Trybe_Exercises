3f68dd0a038dd8f9c0f3787d026cb13b67bfd902
Lily Silva <lilys.prog@gmail.com>
2022-12-20 16:35:35 -0300

    exercício 3

---
+++ fundamentos/bloco8_HigherJS/dia8.5/exercícios/exercise3.js
@@ -0,0 +1,16 @@
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado:
+
+// escreva greet abaixo
+
+const greet = (person, optGreet = 'Hi') => {
+  person === 'John' ? optGreet === 'Good morning' : optGreet === 'Oi'
+  return `${optGreet} ${person}`;
+}
+
+
+
+
+// Retornos esperados:
+console.log(greet('John')); // 'Hi John'
+console.log(greet('John', 'Good morning')); // 'Good morning John'
+console.log(greet('Isabela', 'Oi')); // 'Oi Isabela'
